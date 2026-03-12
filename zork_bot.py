790b05c2852398281abcd1fb95935e3832e6182e
mnky9800n <johnm.aiken@gmail.com>
2026-03-12 19:58:27 +0100

    made it to run multiple docker containers so it can play different games

--- zork_bot.py
+++ zork_bot.py
@@ -4,12 +4,58 @@ import zulip
 import pexpect
 import sys
 
+games = {
+    "abyss": "abyss-r1-s890320.z6",
+    "amfv": "amfv-r77-s850814.z4",
+    "arthur": "arthur-r74-s890714.z6",
+    "ballyhoo": "ballyhoo-r97-s851218.z3",
+    "beyondzork": "beyondzork-r57-s871221.z5",
+    "borderzone": "borderzone-r9-s871008.z5",
+    "bureaucracy": "bureaucracy-r116-s870602.z4",
+    "cutthroats": "cutthroats-r23-s840809.z3",
+    "deadline": "deadline-r27-s831005.z3",
+    "enchanter": "enchanter-r29-s860820.z3",
+    "hitchhiker": "hitchhiker-r60-s861002.z3",
+    "hollywoodhijinx": "hollywoodhijinx-r37-s861215.z3",
+    "infidel": "infidel-r22-s830916.z3",
+    "journey": "journey-r83-s890706.z6",
+    "leathergoddesses": "leathergoddesses-r59-s860730.z3",
+    "lurkinghorror": "lurkinghorror-r203-s870506.z3",
+    "minizork2": "minizork2-r2-s871123.z3",
+    "minizork": "minizork-r34-s871124.z3",
+    "moonmist": "moonmist-r9-s861022.z3",
+    "nordandbert": "nordandbert-r19-s870722.z4",
+    "planetfall": "planetfall-r37-s851003.z3",
+    "plunderedhearts": "plunderedhearts-r26-s870730.z3",
+    "restaurant": "restaurant-r184-s890412.z6",
+    "seastalker": "seastalker-r18-s850919.z3",
+    "sherlock-nosound": "sherlock-nosound-r21-s871214.z5",
+    "sherlock-nosound": "sherlock-nosound-r4-s880324.z5",
+    "sherlock": "sherlock-r26-s880127.z5",
+    "shogun": "shogun-r322-s890706.z6",
+    "sorcerer": "sorcerer-r15-s851108.z3",
+    "spellbreaker": "spellbreaker-r87-s860904.z3",
+    "starcross": "starcross-r17-s821021.z3",
+    "stationfall": "stationfall-r107-s870430.z3",
+    "suspect": "suspect-i190-r18-s850222.z3",
+    "suspended": "suspended-mac-r8-s840521.z3",
+    "trinity": "trinity-r12-s860926.z4",
+    "wishbringer": "wishbringer-r69-s850920.z3",
+    "witness": "witness-r22-s840924.z3",
+    "zork0": "zork0-r393-s890714.z6",
+    "zork1": "zork1-r88-s840726.z3",
+    "zork2": "zork2-r48-s840904.z3",
+    "zork3": "zork3-r17-s840727.z3",
+}
+
 GAME_CHANNEL = os.environ.get("ZORK_CHANNEL", "play-zork")
 GAME_TOPIC = os.environ.get("ZORK_TOPIC", "adventure")
+GAME_NAME = os.environ.get("GAME_NAME")
+GAME_NAME = games[GAME_NAME]
 GAME_PREFIX = "/game"
 SAVE_DIR = "/save"
 # GAME_DAT = os.environ.get("ZORK_DAT", "/home/frotz/DATA/ZORK1.DAT")
-GAME_DAT = os.environ.get("ZORK_DAT", "/home/frotz/DATA/zork1-r88-s840726.z3")
+GAME_DAT = os.environ.get("ZORK_DAT", f"/home/frotz/DATA/{GAME_NAME}")
 
 
 class ZorkSession:
